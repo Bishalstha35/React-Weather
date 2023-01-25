70aec5d63f249fd35486dda9627ca9bee5e4451f
Bishalstha35 <117627174+Bishalstha35@users.noreply.github.com>
2023-01-25 14:23:51 +0545

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '79f204277emsh05300e8f95b0df0p15485fjsn716654d27986',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY ="401d93323ae22e5ca5ca28bb51778220";
